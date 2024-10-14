6dc4948fd3b7db6b5d4f9468f49091be0887f097
diegopevi05 <diego10azul@hotmail.com>
2024-10-14 03:17:13 -0500

    something interesting new

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -14,7 +14,7 @@ const LoadingComponent = ({ isLoading }:any) => {
           transition={{ duration: 2 }}
           className="fixed top-0 left-0 w-full h-full flex flex-col justify-center items-center bg-secondary z-50">
           <div className="loader"></div>
-          <h1 className="font-primary text-white mt-4">{t("Loading...")}</h1>
+          <h1 className="font-primary text-white mt-4">{t("comon.Loading...")}</h1>
         </motion.div>
       )}
     </>
